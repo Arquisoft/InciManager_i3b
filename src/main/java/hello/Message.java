1b672ec2f86f9972eceb2768714edf71b26d1260
UO244522 <fredofuentes96@gmail.com>
2018-03-19 12:49:45 +0100

    Message class modified, to adapt to incident, also index.html

--- src/main/java/hello/Message.java
+++ src/main/java/hello/Message.java
@@ -6,8 +6,18 @@ package hello;
 public class Message {
 
     private String message;
+    private String title;
+    private String location;
 
-    public Message() {}
+    public String getTitle() {
+		return title;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public Message() {}
 
     public String getMessage() {
         return message;
