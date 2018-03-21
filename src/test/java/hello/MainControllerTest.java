f8390e2409d44e16da8ba53ba2b015f168e57cd9
admipa97 <uo251235@uniovi.es>
2018-03-21 23:42:44 +0100

    Nuevos test añadidos en MainControllerTest

--- src/test/java/hello/MainControllerTest.java
+++ src/test/java/hello/MainControllerTest.java
@@ -1,14 +1,14 @@
 package hello;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,11 +21,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import hello.entities.Message;
+import hello.repositories.IncidentsRepository;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class MainControllerTest {
 	
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private IncidentsRepository incidentsRepository;
 
     @Autowired
     private WebApplicationContext context;
@@ -63,4 +69,66 @@ public class MainControllerTest {
 	              .andExpect(status().isOk())
 	              .andExpect(content().string(containsString("LOG IN")));	
 	}
+	
+	@Test
+    public void testGetIndexPage() throws Exception {
+		mockMvc.perform(get("/index")
+				.flashAttr("name", "Pepe")
+				.flashAttr("kind", "1"))
+        		.andExpect(status().isOk())
+        		.andExpect(content().string(containsString("Pepe")))
+        		.andExpect(content().string(containsString("1")))
+        		.andExpect(content().string(containsString("User Page")));		
+	}
+	
+	@Test
+    public void testPostSend() throws Exception {
+//		mockMvc.perform(post("/send")
+//				.sessionAttr("user", "Pepe")
+//				.sessionAttr("kind", "1"))
+//        		.andExpect(status().isOk())
+//        		.andExpect(content().string(containsString("Pepe")))
+//        		.andExpect(content().string(containsString("1")))
+//        		.andExpect(content().string(containsString("User Page")));	
+	}
+	
+	@Test
+    public void testGetList() throws Exception {
+		
+		Message m = new Message();
+		
+		m.setName("Pepe");
+		m.setKind(2);
+		m.setAditionalInfo("Grado 2");
+		m.setLocation("Canada");
+		m.setMessage("Peligro");
+		m.setTitle("Incendio");
+		m.setState(1);
+		String [] s1 = {"tag1", "tag2", "tag3", "tag4"};
+	 	m.setTags(s1);
+	 	HashMap<String, String> customFields = new HashMap<String, String>();
+	 	customFields.put("tempMax", "100 grados");
+	 	m.setCustomFields(customFields);
+	 	
+	 	incidentsRepository.save(m);
+		
+		mockMvc.perform(get("/list")
+				.sessionAttr("user", "Pepe"))
+        		.andExpect(status().isOk())
+        		.andExpect(content().string(containsString("Pepe")))
+        		.andExpect(content().string(containsString("Grado 2")))
+        		.andExpect(content().string(containsString("Canada")))
+        		.andExpect(content().string(containsString("Peligro")))
+        		.andExpect(content().string(containsString("Incendio")))
+        		.andExpect(content().string(containsString("1")))
+        		.andExpect(content().string(containsString("tag1")))
+        		.andExpect(content().string(containsString("tag2")))
+        		.andExpect(content().string(containsString("tag3")))
+        		.andExpect(content().string(containsString("tempMax")))
+        		.andExpect(content().string(containsString("100 grados")))
+        		.andExpect(content().string(containsString("tag4")));	
+		
+		incidentsRepository.delete(m);
+	}
+	
 }
